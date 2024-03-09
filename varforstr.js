ef0c804c0d92ab21d6efae37008c35bf22788340
Samar-Memon <samar56786raza@gmail.com>
2024-03-09 22:34:07 +0500

    Js-Assignment-Var4Str

---
+++ varforstr.js
@@ -0,0 +1,15 @@
+var firstName = "Samar";
+var lastName = "Raza";
+var age = "15";
+var email = "samarraza123@gmail.com";
+var learNing = "Web and Mobile App Development";
+alert("My FirstName is "+ firstName)
+document.write("My FirstName is "+ firstName) + "<br>" + "<br>"
+alert("My LastName is "+ lastName)
+document.write("My LastName is "+ lastName + "<br>" + "<br>")
+alert("My Age is "+ age)
+document.write("My Age is "+ age + "<br>" + "<br>")
+alert("My Email is "+ email)
+document.write("My Email is "+ email + "<br>" + "<br>")
+alert("I Learn "+ learNing)
+document.write("I Learn "+ learNing + "<br>" + "<br>")
